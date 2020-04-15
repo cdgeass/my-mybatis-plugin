5a4c76798d7d92ab3846a90e57687dc2f67d3a12
cdgeass <cdgeass97@gmail.com>
2020-04-15 08:20:50 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    id("org.jetbrains.intellij") version "0.4.18"
+    java
+}
+
+group = "io.github.cdgeass"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testCompile("junit", "junit", "4.12")
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version = "2020.1"
+}
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    changeNotes("""
+      Add change notes here.<br>
+      <em>most HTML tags may be used</em>""")
+}
---
+++ src/main/java/io/github/cdgeass/toolwindow/MyBatisLogFormatterToolWindow.java
@@ -0,0 +1,17 @@
+package io.github.cdgeass.toolwindow;
+
+import javax.swing.*;
+
+/**
+ * @author cdgeass
+ * @since  2020-04-14
+ */
+public class MyBatisLogFormatterToolWindow {
+    private JButton cleanButton;
+    private JScrollPane scrollLogPanel;
+    private JPanel toolWindow;
+
+    public JPanel getContent() {
+        return toolWindow;
+    }
+}
