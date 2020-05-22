7d5b3fdd54dcaf3ce5fceee4a10678a57ab24571
cdgeass <cdgeass97@gmail.com>
2020-05-22 18:37:44 +0800

    modify version information

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "io.github.cdgeass"
-version = "1.0.6-alpha"
+version = "1.1.1-alpha"
 
 repositories {
     mavenCentral()
@@ -26,16 +26,9 @@ configure<JavaPluginConvention> {
 }
 tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
     changeNotes("""
-    1.0.7-alpha<br>
+    1.1.1-alpha<br>
     <ul>
-        <li>add mapper navigation</li>
-        <li>modify sql dialog font to Jetbrains Mono</li>
-        <li>fix issues</>
-    </ul>
-    
-    1.0.1<br>
-    <ul>
-        <li>add log format</li>
+        <li>add xml reference</li>
     </ul>
       """)
 }
