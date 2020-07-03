9934310555e819eb1547883afcd473b2a4fd67e8
cdgeass <cdgeass97@gmail.com>
2020-07-03 15:30:54 +0800

    add property completion

--- src/main/java/io/github/cdgeass/constants/StringConstants.java
+++ src/main/java/io/github/cdgeass/constants/StringConstants.java
@@ -17,9 +17,12 @@ public class StringConstants {
     public final static String CONFIGURATION = "configuration";
     public final static String MAPPER = "mapper";
     public final static String NAMESPACE = "namespace";
-    public final static String RESULTMAP = "resultMap";
+    public final static String RESULT_MAP = "resultMap";
 
     public final static String TYPE = "type";
+    public final static String PROPERTY = "property";
+    public final static String COLUMN = "column";
+
 
     private StringConstants() {
 
--- src/main/java/io/github/cdgeass/editor/dom/contributor/MapperCompletionContributor.java
+++ src/main/java/io/github/cdgeass/editor/dom/contributor/MapperCompletionContributor.java
@@ -1,10 +1,14 @@
 package io.github.cdgeass.editor.dom.contributor;
 
+import com.google.common.base.CaseFormat;
 import com.google.common.collect.Lists;
 import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.xml.XmlAttribute;
+import com.intellij.psi.xml.XmlAttributeValue;
 import com.intellij.psi.xml.XmlTag;
 import com.intellij.util.ProcessingContext;
 import io.github.cdgeass.constants.StringConstants;
@@ -22,8 +26,6 @@ public class MapperCompletionContributor extends CompletionContributor {
 
     private static final List<String> CONTAINS_TYPE = Lists.newArrayList("resultMap");
 
-    private static final List<String> CONTAINS_PROPERTY = Lists.newArrayList("id", "result");
-
     public MapperCompletionContributor() {
         extend(
                 CompletionType.BASIC,
@@ -34,33 +36,79 @@ public class MapperCompletionContributor extends CompletionContributor {
                                                   @NotNull ProcessingContext context,
                                                   @NotNull CompletionResultSet result) {
                         var position = parameters.getPosition();
-                        var xmlTag = (XmlTag) PsiTreeUtil.findFirstParent(position,
-                                psiElement -> psiElement instanceof XmlTag && CONTAINS_TYPE.contains(((XmlTag) psiElement).getName()));
-                        if (xmlTag == null) {
+                        var isContinue = position.getParent() != null && position.getParent() instanceof XmlAttributeValue
+                                && position.getParent().getParent() != null && position.getParent().getParent() instanceof XmlAttribute;
+                        if (!isContinue) {
                             return;
                         }
 
-                        var type = xmlTag.getAttributeValue(StringConstants.TYPE);
-                        if (type == null) {
-                            return;
+                        var attributeName = ((XmlAttribute)position.getParent().getParent()).getName();
+                        if (StringConstants.COLUMN.equals(attributeName)) {
+                            addColumnKeyWords(parameters, result);
                         }
-                        var project = parameters.getEditor().getProject();
-                        if (project == null) {
-                            return;
-                        }
-
-                        var javaPsiFacade = JavaPsiFacade.getInstance(project);
-                        var psiClass = javaPsiFacade.findClass(type, GlobalSearchScope.allScope(project));
-                        if (psiClass == null) {
-                            return;
+                        if (StringConstants.PROPERTY.equals(attributeName)) {
+                            addPropertyKeyWords(parameters, result);
                         }
 
-                        var psiFields = psiClass.getAllFields();
-
-                        return;
                     }
                 }
         );
+    }
+
+    private static void addPropertyKeyWords(CompletionParameters parameters, CompletionResultSet result) {
+        var xmlTag = (XmlTag) PsiTreeUtil.findFirstParent(parameters.getPosition(),
+                psiElement -> psiElement instanceof XmlTag && CONTAINS_TYPE.contains(((XmlTag) psiElement).getName()));
+        if (xmlTag == null) {
+            return;
+        }
+
+        var type = xmlTag.getAttributeValue(StringConstants.TYPE);
+        if (type == null) {
+            return;
+        }
+        var project = parameters.getEditor().getProject();
+        if (project == null) {
+            return;
+        }
+
+        var javaPsiFacade = JavaPsiFacade.getInstance(project);
+        var psiClass = javaPsiFacade.findClass(type, GlobalSearchScope.allScope(project));
+        if (psiClass == null) {
+            return;
+        }
+
+        var psiFields = psiClass.getAllFields();
+        for (var psiField : psiFields) {
+            result.addElement(LookupElementBuilder.create(psiField.getName()));
+        }
+    }
+
+    private static void addColumnKeyWords(CompletionParameters parameters, CompletionResultSet result) {
+        var xmlTag = (XmlTag) PsiTreeUtil.findFirstParent(parameters.getPosition(),
+                psiElement -> psiElement instanceof XmlTag && CONTAINS_TYPE.contains(((XmlTag) psiElement).getName()));
+        if (xmlTag == null) {
+            return;
+        }
+
+        var type = xmlTag.getAttributeValue(StringConstants.TYPE);
+        if (type == null) {
+            return;
+        }
+        var project = parameters.getEditor().getProject();
+        if (project == null) {
+            return;
+        }
+
+        var javaPsiFacade = JavaPsiFacade.getInstance(project);
+        var psiClass = javaPsiFacade.findClass(type, GlobalSearchScope.allScope(project));
+        if (psiClass == null) {
+            return;
+        }
+
+        var psiFields = psiClass.getAllFields();
+        for (var psiField : psiFields) {
+            result.addElement(LookupElementBuilder.create(CaseFormat.UPPER_CAMEL.to(CaseFormat.LOWER_UNDERSCORE, psiField.getName())));
+        }
     }
 
     @Override
