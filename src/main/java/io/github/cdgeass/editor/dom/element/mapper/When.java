de3fbd7a529eaa37d339c0d65a8d41501c64fdff
cdgeass <cdgeass97@gmail.com>
2020-08-31 10:51:45 +0800

    fix(Dom): Add DomElement

--- src/main/java/io/github/cdgeass/editor/dom/element/mapper/When.java
+++ src/main/java/io/github/cdgeass/editor/dom/element/mapper/When.java
@@ -2,12 +2,39 @@ package io.github.cdgeass.editor.dom.element.mapper;
 
 import com.intellij.util.xml.DomElement;
 import com.intellij.util.xml.GenericAttributeValue;
+import com.intellij.util.xml.SubTagList;
+
+import java.util.List;
 
 /**
  * @author cdgeass
- * @since  2020-06-20
+ * @since 2020-06-20
  */
 public interface When extends DomElement {
 
     GenericAttributeValue<String> getTest();
+
+    @SubTagList("include")
+    List<Include> getIncludes();
+
+    @SubTagList("trim")
+    List<Trim> getTrims();
+
+    @SubTagList("where")
+    List<Where> getWheres();
+
+    @SubTagList("set")
+    List<Set> getSets();
+
+    @SubTagList("foreach")
+    List<Foreach> getForeaches();
+
+    @SubTagList("choose")
+    List<Choose> getChooses();
+
+    @SubTagList("if")
+    List<If> getIfs();
+
+    @SubTagList("bind")
+    List<Bind> getBinds();
 }
