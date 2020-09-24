ebc7c73e41ef6128c97882f2f55f59f852b0cdbd
cdgeass <cdgeass97@gmail.com>
2020-09-24 18:53:33 +0800

    feature(Action): Register action
    Register action in DatabaseView

---
+++ src/main/kotlin/io/github/cdgeass/action/MyBatisGeneratorAction.kt
@@ -0,0 +1,15 @@
+package io.github.cdgeass.action
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+/**
+ * @author cdgeass
+ * @since  2020-09-24
+ */
+class MyBatisGeneratorAction : AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+
+    }
+}
