0a0b9794cabb9b146b5e971c4d689be9d8fa22ca
cdgeass <cdgeass97@gmail.com>
2020-11-04 15:40:11 +0800

    feature(Settings UI): Context properties table

--- src/main/kotlin/io/github/cdgeass/component/MyBatisGeneratorSettingsComponent.kt
+++ src/main/kotlin/io/github/cdgeass/component/MyBatisGeneratorSettingsComponent.kt
@@ -2,8 +2,11 @@ package io.github.cdgeass.component
 
 import com.intellij.openapi.ui.ComboBox
 import com.intellij.ui.TitledSeparator
+import com.intellij.ui.ToolbarDecorator
 import com.intellij.ui.components.JBLabel
+import com.intellij.ui.table.TableView
 import com.intellij.util.ui.FormBuilder
+import org.apache.commons.lang3.tuple.MutablePair
 import javax.swing.JPanel
 
 /**
@@ -17,14 +20,29 @@ class MyBatisGeneratorSettingsComponent {
     private val contextDefaultModelTypeComboBox: ComboBox<String> = ComboBox(arrayOf("conditional", "flat", "hierarchical"))
     private val contextTargetRuntimeComboBox: ComboBox<String> = ComboBox(arrayOf("MyBatis3DynamicSql",
             "MyBatis3Kotlin", "MyBatis3", "MyBatis3Simple", "MyBatis3DynamicSqlV1"))
-    private val contextPropertiesModel: PropertiesTableModel = PropertiesTableModel()
+    private val contextPropertiesTableModel: PropertiesTableModel = PropertiesTableModel(arrayOf("autoDelimitKeywords",
+            "beginningDelimiter", "endingDelimiter", "javaFileEncoding", "javaFormatter", "targetJava8", "kotlinFileEncoding",
+            "kotlinFormatter", "xmlFormatter"))
 
     init {
+        val contextPropertiesTable = TableView(contextPropertiesTableModel).let {
+            it.emptyText.text = "There has no properties"
+            it
+        }
+        val contextToolbarDecorator = ToolbarDecorator.createDecorator(contextPropertiesTable)
+                .setAddAction {
+                    contextPropertiesTableModel.addRow(MutablePair("", ""))
+                }
+                .setRemoveAction {
+                    contextPropertiesTableModel.removeRow(contextPropertiesTable.selectedRow)
+                }
 
         val contextPanel = FormBuilder.createFormBuilder()
                 .addComponent(TitledSeparator("Context"))
                 .addLabeledComponent(JBLabel("DefaultModelType:"), contextDefaultModelTypeComboBox)
                 .addLabeledComponent(JBLabel("TargetRuntime:"), contextTargetRuntimeComboBox)
+                .addComponent(TitledSeparator("Context properties"))
+                .addComponent(contextToolbarDecorator.createPanel())
                 .addComponentFillVertically(JPanel(), 0)
                 .panel
 
--- src/main/kotlin/io/github/cdgeass/component/PropertiesTableModel.kt
+++ src/main/kotlin/io/github/cdgeass/component/PropertiesTableModel.kt
@@ -1,26 +1,52 @@
 package io.github.cdgeass.component
 
+import com.intellij.openapi.ui.ComboBox
 import com.intellij.util.ui.ColumnInfo
 import com.intellij.util.ui.ListTableModel
+import org.apache.commons.lang3.tuple.MutablePair
+import javax.swing.DefaultCellEditor
+import javax.swing.table.TableCellEditor
 
 /**
  * @author cdgeass
  * @since 2020-09-30
  */
-class PropertiesTableModel : ListTableModel<Pair<String, String>>()
+class PropertiesTableModel(properties: Array<String>) : ListTableModel<MutablePair<String, String>>(PropertyColumnInfo(properties), ValueColumnInfo())
 
-class FirstColumnInfo(name: String?) : ColumnInfo<Pair<String, String>, String>(name) {
+class PropertyColumnInfo(private val properties: Array<String>) : ColumnInfo<MutablePair<String, String>, String>("property") {
 
-    override fun valueOf(item: Pair<String, String>?): String? {
-        return item?.first
+    override fun valueOf(item: MutablePair<String, String>): String {
+        return item.left
     }
 
+    override fun isCellEditable(item: MutablePair<String, String>): Boolean {
+        return true
+    }
+
+    override fun setValue(item: MutablePair<String, String>, value: String) {
+        if (properties.contains(value)) {
+            item.left = value
+        } else {
+            item.left = properties[0]
+        }
+    }
+
+    override fun getEditor(item: MutablePair<String, String>): TableCellEditor {
+        return DefaultCellEditor(ComboBox(properties))
+    }
 }
 
-class SecondColumnInfo(name: String?) : ColumnInfo<Pair<String, String>, String>(name) {
+class ValueColumnInfo : ColumnInfo<MutablePair<String, String>, String>("value") {
 
-    override fun valueOf(item: Pair<String, String>?): String? {
-        return item?.second
+    override fun valueOf(item: MutablePair<String, String>): String {
+        return item.right
     }
 
+    override fun isCellEditable(item: MutablePair<String, String>): Boolean {
+        return true
+    }
+
+    override fun setValue(item: MutablePair<String, String>, value: String) {
+        item.right = value
+    }
 }
