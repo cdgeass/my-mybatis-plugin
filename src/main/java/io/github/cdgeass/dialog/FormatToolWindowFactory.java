c8b245cb7cee2060c78832accf7f82c3df45d9da
cdgeass <cdgeass97@gmail.com>
2020-12-07 13:56:06 +0800

    fix(Icons): Fix can not load icon

--- src/main/java/io/github/cdgeass/dialog/FormatToolWindowFactory.java
+++ src/main/java/io/github/cdgeass/dialog/FormatToolWindowFactory.java
@@ -4,9 +4,6 @@ import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
-import com.intellij.ui.content.ContentFactory;
-import com.intellij.ui.content.ContentManager;
-import io.github.cdgeass.constants.MyIcons;
 import org.jetbrains.annotations.NotNull;
 
 /**
