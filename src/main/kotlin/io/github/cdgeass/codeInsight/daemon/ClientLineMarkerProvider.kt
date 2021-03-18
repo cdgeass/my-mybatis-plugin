b1b04d10c3c8b5e0ab2113b0b3de61c32c71c8bc
cdgeass <cdgeass97@gmail.com>
2021-03-18 11:23:39 +0800

    refactor(LineMarkerProvider): Rewrite client lineMarker provider

--- src/main/kotlin/io/github/cdgeass/codeInsight/daemon/ClientLineMarkerProvider.kt
+++ src/main/kotlin/io/github/cdgeass/codeInsight/daemon/ClientLineMarkerProvider.kt
@@ -7,6 +7,8 @@ import com.intellij.icons.AllIcons
 import com.intellij.psi.PsiClass
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiIdentifier
+import com.intellij.psi.PsiMethod
+import com.intellij.psi.util.PsiTreeUtil
 import com.intellij.psi.util.elementType
 import com.intellij.psi.xml.XmlTag
 import com.intellij.psi.xml.XmlToken
@@ -28,38 +30,43 @@ class ClientLineMarkerProvider : RelatedItemLineMarkerProvider() {
         element: PsiElement,
         result: MutableCollection<in RelatedItemLineMarkerInfo<*>>
     ) {
-        val parent = element.parent
-        if (element !is PsiIdentifier || parent !is PsiClass) {
+        if (element !is PsiIdentifier) {
             return
         }
 
-        val targets = collectTargets(element, parent)
-        if (targets.isEmpty()) {
-            return
-        }
+        val lineMarkerInfo: RelatedItemLineMarkerInfo<PsiElement> = when (val parent = element.parent) {
+            is PsiClass -> {
+                collectTargets(element, parent)
+            }
+            is PsiMethod -> {
+                collectTarget(element, parent)
+            }
+            else -> {
+                null
+            }
+        } ?: return
 
-        result.add(
-            NavigationGutterIconBuilder.create(AllIcons.Gutter.OverridenMethod)
-                .setTargets(targets)
-                .createLineMarkerInfo(element)
-        )
+        result.add(lineMarkerInfo)
     }
 
-    private fun collectTargets(element: PsiIdentifier, parent: PsiClass): List<PsiElement> {
-        val qualifiedName = parent.qualifiedName ?: return emptyList()
+    /**
+     * 接口
+     */
+    private fun collectTargets(element: PsiIdentifier, parent: PsiClass): RelatedItemLineMarkerInfo<PsiElement>? {
+        val qualifiedName = parent.qualifiedName ?: return null
         val xmlFiles = findByNamespace(qualifiedName, element.project)
 
-        return xmlFiles.map { xmlFile ->
+        val targets = xmlFiles.map { xmlFile ->
             val xmlTokens = mutableListOf<XmlToken>()
             XmlPsiUtil.processXmlElementChildren(
                 xmlFile,
                 { psiElement ->
-                    // 是否 mapper 标签
-                    if (psiElement is XmlToken && psiElement.text == "mapper" && psiElement.parent is XmlTag) {
-                        // 是否开始标签
-                        if (psiElement.prevSibling.elementType == XmlTokenType.XML_START_TAG_START) {
-                            xmlTokens.add(psiElement)
-                        }
+                    if (psiElement is XmlToken &&
+                        psiElement.prevSibling.elementType == XmlTokenType.XML_START_TAG_START &&
+                        psiElement.parent is XmlTag &&
+                        psiElement.text == "mapper"
+                    ) {
+                        xmlTokens.add(psiElement)
                     }
                     true
                 },
@@ -67,6 +74,43 @@ class ClientLineMarkerProvider : RelatedItemLineMarkerProvider() {
             )
             xmlTokens
         }.flatten()
+
+        return NavigationGutterIconBuilder.create(AllIcons.Gutter.OverridenMethod)
+            .setTargets(targets)
+            .createLineMarkerInfo(element)
+    }
+
+    /**
+     * 方法
+     */
+    private fun collectTarget(element: PsiIdentifier, parent: PsiMethod): RelatedItemLineMarkerInfo<PsiElement>? {
+        val psiClass = (PsiTreeUtil.findFirstParent(parent) { it is PsiClass } ?: return null) as PsiClass
+        val qualifiedName = psiClass.qualifiedName ?: return null
+
+        val xmlFiles = findByNamespace(qualifiedName, element.project)
+
+        val targets = xmlFiles.map { xmlFile ->
+            val xmlTokens = mutableListOf<XmlToken>()
+            XmlPsiUtil.processXmlElementChildren(
+                xmlFile,
+                { psiElement ->
+                    if (psiElement is XmlToken &&
+                        psiElement.prevSibling.elementType == XmlTokenType.XML_START_TAG_START &&
+                        psiElement.parent is XmlTag &&
+                        (psiElement.parent as XmlTag).getAttributeValue("id") == element.text
+                    ) {
+                        xmlTokens.add(psiElement)
+                    }
+                    true
+                },
+                true
+            )
+            xmlTokens
+        }.flatten()
+
+        return NavigationGutterIconBuilder.create(AllIcons.Gutter.ImplementedMethod)
+            .setTargets(targets)
+            .createLineMarkerInfo(element)
     }
 
 }
