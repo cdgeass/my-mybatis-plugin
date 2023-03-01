8099fca31fdcfe27ff92a3b6465e9a2593199e26
cdgeass <cdgeass97@gmail.com>
2023-03-01 17:27:37 +0800

    Update Icons

---
+++ src/main/kotlin/icons/MyIcons.kt
@@ -0,0 +1,15 @@
+package icons
+
+import com.intellij.openapi.util.IconLoader
+
+object MyIcons {
+
+    @JvmField
+    val Action = IconLoader.getIcon("/icons/actionMybatis.png", javaClass)
+
+    @JvmField
+    val ToolWindow = IconLoader.getIcon("/icons/toolWindowMybatis.png", javaClass)
+
+    @JvmField
+    val Gutter = IconLoader.getIcon("/icons/gutterMybatis.png", javaClass)
+}
